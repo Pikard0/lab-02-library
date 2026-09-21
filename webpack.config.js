e286be7c83fbc454067a85e3477c24b433edc1da
artempostarenko@gmail.com <artempostarenko@gmail.com>
2026-09-21 10:45:19 +0300

    5), 608 bytes | 60

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,12 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 const path = require('path');
-
 module.exports = {
   entry: './src/app.ts',
+  devtool: 'source-map',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    publicPath: '/lab-02-library/',
     clean: true
   },
   resolve: {
